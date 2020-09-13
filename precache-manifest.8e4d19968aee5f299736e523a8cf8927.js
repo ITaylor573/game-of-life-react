59b6c901d9a735463cc96ba6e8f916a97f1b86aa
Inigo Taylor <inigotaylor573@gmail.com>
2020-09-13 17:13:25 +0100

    Updates

---
+++ precache-manifest.8e4d19968aee5f299736e523a8cf8927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfba6a98c22f85d40cf103e11b3ec259",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "c5a4cf271abb383c2f32",
+    "url": "/game-of-life/static/css/main.0ce2cd30.chunk.css"
+  },
+  {
+    "revision": "fa8c09fe2afc154fea7e",
+    "url": "/game-of-life/static/js/2.34538766.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.34538766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5a4cf271abb383c2f32",
+    "url": "/game-of-life/static/js/main.b4d64939.chunk.js"
+  },
+  {
+    "revision": "85de1be1c6cd64413302",
+    "url": "/game-of-life/static/js/runtime-main.b93f419f.js"
+  }
+]);
